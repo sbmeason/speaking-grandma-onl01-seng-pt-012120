873a19f046f5c0a18c205ccb8ef9d856a155fe62
Samantha Meason <sbmeason@gmail.com>
2020-01-26 23:30:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase 
+  elsif phrase ! = 
  else
    "NO, NOT SINCE 1938!"
   end
