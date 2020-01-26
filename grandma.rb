38a6bf917619c6f6e5ede5a7ad26eea524c14b87
Samantha Meason <sbmeason@gmail.com>
2020-01-26 23:01:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,9 @@ speak_to_grandma = "I LOVE YOU GRANDMA!"
 if speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 end
-speak_to_grandma
+
+if speak_to_grandma == "I love you grandma!"
+  puts "HUH?! SPEAK UP,SONNY!"
+  
 
   
